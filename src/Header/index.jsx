fc982d85b4a28852cd513db57a1ce39e01b199d9
Reetika001 <rtkchandrakar@gmail.com>
2024-09-12 14:39:03 +0530

    changes

--- src/Header/index.jsx
+++ src/Header/index.jsx
@@ -5,7 +5,7 @@ export class Header extends Component {
   render() {
     return (
       <View>
-        <Text>Header modify</Text>
+        <Text>Header modified</Text>
       </View>
     );
   }
