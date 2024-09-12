504d001d21f815d5572fc674c0418a305b43705f
NidaJavedAlvi <786nidajaved@gmail.com>
2024-09-12 11:21:45 +0530

    added footer

---
+++ src/Footer/index.jsx
@@ -0,0 +1,14 @@
+import { Text, View } from "react-native";
+import React, { Component } from "react";
+
+export class Footer extends Component {
+  render() {
+    return (
+      <View>
+        <Text>Footer`</Text>
+      </View>
+    );
+  }
+}
+
+export default Footer;
